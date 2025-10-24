d3586c778f08d8d67df24f50e6e3e46917f33322
appellatebaton0@gmail.com <corneilusmaxfield@gmail.com>
2025-10-23 19:39:36 -0500

    v0.4 - HealthBits and Finding Bits
    - Made HealthBits, which provide health functionality (max health, taking damage and regenerating health, etc.)
    - Made a function in Bots to recursively search for certain Bits in their children; get_sub_bit. It checks itself, its children, then runs the function using the children as the basis instead of itself, for 4 layers by default.

---
+++ scripts/bits/health.gd
@@ -0,0 +1,29 @@
+class_name HealthBit extends Bit
+## Provides all the functionality expected of a Bot that would have health.
+## Signals on taking damage and health reaching zero, etc.
+
+signal took_damage ## Emitted when the health value decreases.
+signal took_regeneration ## Emitted when the health value increases.
+signal reached_zero ## Emitted when the health value reaches zero.
+
+@export var max_health := 40.0
+@export var health := 40.0
+
+## Called by things that want to do damage, namely DamageBits.
+func modify_health(amount:float):
+	if amount == 0:
+		return
+	elif amount < 0: ## If the modification is damage, take the damage
+		health = max(0, health + amount)
+		took_damage.emit()
+	else: ## If the modification is regen, regenerate the health.
+		health = min(max_health, health + amount)
+		took_regeneration.emit()
+	
+	if health <= 0:
+		reached_zero.emit()
+
+
+@export var t:Bot
+func _ready() -> void:
+	print(t.get_sub_bit("HealthBit", true))
--- scripts/bot.gd
+++ scripts/bot.gd
@@ -8,3 +8,30 @@ func bot_free():
 
 func bot_free_val(_x):
 	bot_free()
+
+## Get a bit in the children, using its class_name.
+func get_sub_bit(bit_id:String, include_self := false, depth := 4, with:Node = self) -> Bit:
+	
+	if depth <= 0:
+		return null
+	
+	# If can return self and self works, do that.
+	if include_self and get_script().get_global_name() == bit_id:
+		return self
+	
+	# Otherwise, look in the children recursively
+	for child in with.get_children():
+		# Try this child, first.
+		if child is Bit:
+			# If its class_name matches, return it.
+			if child.get_script().get_global_name() == bit_id and child.bot == self:
+				return child
+		
+		# Else, run this function for each Bit child, with one less recursion so it's not infinite.
+		var attempt = get_sub_bit(bit_id, true, depth - 1, child)
+		
+		# If anything is found, return that.
+		if attempt != null:
+			return attempt
+	
+	return null	
