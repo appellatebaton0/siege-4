a6598b566bca477be9a5324236fffd8c30a562c5
appellatebaton0@gmail.com <corneilusmaxfield@gmail.com>
2025-10-26 12:56:47 -0500

    v.0.11 - Score!
    - The score is fully working, now I just need to do the highscore and make them both visible on the lose screen. Final push time!

--- scripts/values/float/round.gd
+++ scripts/values/float/round.gd
@@ -1,6 +1,16 @@
-class_name ExampleValue extends Value
-## What do I do?
+class_name RoundedFloat extends FloatValue
+## Rounds a float to a certain place.
 
-func value() -> Variant:
-	return null
+## The float to round.
+@export var input:FloatValue
 
+## The places to round it to.
+@export var places:int
+
+func value() -> float:
+	if input != null:
+		var place_value := pow(10, places)
+		
+		return round(input.value() * place_value) / place_value
+	
+	return -1.0
